d8f56373d33b2a7295cdf7f36734ae2c91137ea3
iqbal707 <iqbal70K@gmail.com>
2025-08-29 11:53:22 +0600

    dynamic call section completed

--- script.js
+++ script.js
@@ -14,4 +14,38 @@
          })
   }
 
-// ==-== Event listener for call icon ==-== //
+
+   // ==-== Event listener for call icon ==-== //
+
+      const callIcon = document.getElementsByClassName('call-icon');
+   
+       
+       for( const call of callIcon) {
+             
+              call.addEventListener('click', function() {
+
+             let coinCount = parseInt(document.getElementById('coin-count')
+                      .innerText);      
+
+             if ( coinCount >= 20) { 
+
+              document.getElementById('coin-count')
+                     .innerText = coinCount - 20; 
+
+                   const instituteName = call.parentNode.parentNode.children[0].children[1].innerText;
+                   
+                   const instituteNum = call.parentNode.parentNode.children[1].innerText;
+
+                   alert(`You are calling ${instituteName} - ${instituteNum}`)
+
+             } else {
+                alert ('sorry you dont have sufficient coin for calling')
+             }
+                   
+              } )
+
+
+       }
+
+
+ 
